06decf1f957f1d04bdd944e3915b71ee1de82e31
alejandroatacho <themoonbunnys@gmail.com>
2022-01-18 20:10:22 +0000

    first upload

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,25 @@
+# _*_ coding utf-8 _*_
+{
+    'name: 'Odoo Academy',
+    'summary': """academy app to manage training""",
+    'description': """
+        Academy Module to manage Training:
+        -vCourses
+        - Sessions
+        -Attendees
+    """,
+    'author': 'Odoo',
+    
+    'website':'smart-obc.com',
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ]
+    
+}
